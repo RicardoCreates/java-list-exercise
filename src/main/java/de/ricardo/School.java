b6042a405342eeec52e28deeba6f05f82f4df560
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-28 11:47:12 +0200

    fix

--- src/main/java/de/ricardo/School.java
+++ src/main/java/de/ricardo/School.java
@@ -7,15 +7,13 @@ import java.util.Map;
 
 public class School {
     //private List<Student> students;
-    private Map<String, Student> students;
+    private Map<String, Student> students = new HashMap<>();
 
     //public School() {
     //    this.students = new ArrayList<>();
     //}
 
-    public School() {
-        students = new HashMap<>();
-    }
+
 
     //public void addStudent(Student student) {
     //    students.add(student);
